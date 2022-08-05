78ac3439647b2375d62360e6e638f9d77b219ac6
AdSlb <adley.salabi@gmail.com>
2022-08-05 10:15:10 +0200

    feat(user-input) : created comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule,
